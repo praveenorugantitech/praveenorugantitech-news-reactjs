ceea69544312806597b55d758d941ce9e2657977
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-02-01 19:18:46 +0530

    updated

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import TopStory from './TopStory';
 import Spinner from './layouts/Spinner';
 import Grid from "@material-ui/core/Grid";
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
 import { NavLink } from "react-router-dom";
+
 
 const useStyles = makeStyles((theme) => ({
     card: {
@@ -27,8 +28,10 @@ const useStyles = makeStyles((theme) => ({
 
 
 function TopStories({ loading, topStories, getTopArticles }) {
+    const [heading,setHeading] = useState("");
     const classes = useStyles();
     useEffect(() => {
+        setHeading("World News");
         getTopArticles('world');
     }, []);
 
@@ -40,17 +43,19 @@ function TopStories({ loading, topStories, getTopArticles }) {
                 ) : (
                 <>
                     <div className={classes.buttons}>
-                        <Button onClick={() => { getTopArticles('world'); }} variant="outlined" color="primary">World News</Button>
-                        <Button onClick={() => { getTopArticles('us'); }} variant="outlined" color="default">US News</Button>
-                        <Button onClick={() => { getTopArticles('politics'); }} variant="outlined" style={{color:'brown'}}>Politics</Button>
-                        <Button onClick={() => { getTopArticles('technology'); }} variant="outlined" color="secondary">Technology</Button>
-                        <Button onClick={() => { getTopArticles('movies'); }} variant="outlined" style={{color:'green'}}>Movies</Button>
-                        <Button onClick={() => { getTopArticles('food'); }} variant="outlined" style={{color:'orange'}}>Food</Button>
+                        <Button onClick={() => {setHeading("World News"); getTopArticles('world'); }} variant="outlined" color="primary">World News</Button>
+                        <Button onClick={() => {setHeading("US News"); getTopArticles('us'); }} variant="outlined" color="default">US News</Button>
+                        <Button onClick={() => {setHeading("Politics");  getTopArticles('politics'); }} variant="outlined" style={{color:'brown'}}>Politics</Button>
+                        <Button onClick={() => {setHeading("Technology");  getTopArticles('technology'); }} variant="outlined" color="secondary">Technology</Button>
+                        <Button onClick={() => {setHeading("Movies"); getTopArticles('movies'); }} variant="outlined" style={{color:'green'}}>Movies</Button>
+                        <Button onClick={() => {setHeading("Food");  getTopArticles('food'); }} variant="outlined" style={{color:'orange'}}>Food</Button>
                     </div>
 
                     <NavLink to="/">
                         <Link component="button" variant="body2">Go Back</Link>
                     </NavLink>
+
+                    <h6 className="MuiTypography-h6">{heading}</h6>
 
                     <div className={classes.root} >
                         <Grid container spacing={3}>
